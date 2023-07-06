492865a89b067efc62c78b4462c5bd1295d87e3f
Abhishek Choudhury <abhishek.choudhury2697@gmail.com>
2023-07-06 09:27:36 +0530

    solve 01-easy-todo-app

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react';
 import './App.css'
+import axios from 'axios';
+import PropTypes from "prop-types";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000',
+})
+
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+    const [todos, setTodos] = useState([])
 
-  return (
-    <>
-      <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
-      </div>
-    </>
-  )
+    const fetchTodos = () => {
+        instance.get('/todos')
+            .then(response => {
+                setTodos(response.data);
+            })
+            .catch(error => console.log(error));
+    }
+
+    const deleteTodo = id => {
+        axios.delete(`http://localhost:3000/todos/${id}`)
+            .then(response => {
+                console.log(response);
+                console.log(`Todo: ${id} deleted`);
+                fetchTodos();
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchTodos()
+    }, []);
+
+
+    return (
+        <>
+            <div>
+                {todos.map(todo => <Todo key={todo.id} deleteTodo={deleteTodo} {...todo}/>)}
+            </div>
+        </>
+    )
 }
 
-function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
+function Todo({id, title, description, deleteTodo}) {
+    return (
+        <div id={id}>
+            <div className='title'>{title}</div>
+            <div className='description'>{description}</div>
+            <button onClick={() => deleteTodo(id)}>Delete</button>
+        </div>
+    )
 }
+
+Todo.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    deleteTodo: PropTypes.func.isRequired,
+};
 
 export default App
